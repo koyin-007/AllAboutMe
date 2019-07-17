d85f993e24bb568f503d06456c64385f5989ed47
unknown <koyinsola007@gmail.com>
2019-07-17 10:33:25 -0500

    First commit

---
+++ myscript.js
@@ -0,0 +1,41 @@
+// var person = prompt("Please enter your name", "Harry Potter");
+
+if (person = "Mariam"){
+  "Welcome back"
+}
+if (person != "Mariam") {
+  document.getElementById("demo").innerHTML =
+  alert("Welcome to this page " + person + "! How are you today?");
+}
+
+function showTime(){
+  document.getElementById('demo').innerHTML = Date();
+}
+//
+// var name;
+// name = "Mariam";
+// name = "Yasmine";
+// function timesFive(num) {return num * 5};
+//
+// function helloWorld() {alert("Hello World") };
+//
+// if (5==5) {
+//   alert("Yay!");
+// }
+//
+// var age = 16;
+// age = age +1;
+//
+// let text = ""
+//
+// for (var i = 0; i < 5; i++) {
+//   text += "Num: "+i;
+// }
+//
+// let num = 0
+// for (var i = 0; i < 5; i++){
+//   num += i
+// }
+// console.log("Hello World");
+// debugger;
+// console.log(num);
